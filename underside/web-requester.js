21d9f561efd6ed66625c0708cda31245a2a0cec5
MaySoMusician <maysomusician@gmail.com>
2019-02-04 20:35:13 +0900

    Use Querystring module, define user-agent inside function

--- underside/web-requester.js
+++ underside/web-requester.js
@@ -1,18 +1,18 @@
 /* eslint-disable one-var */
-const rpn = require('request-promise-native'),
-      USERAGENT = 'Request-Promise-Native/1.0.5';
+const querystring = require('querystring'),
+      rpn = require('request-promise-native');
+      // USERAGENT = 'Request-Promise-Native/1.0.5';
 
 const get = (MFBGB, uri, queryStrings, parseJson) => {
-  let strQueryString = '';
+  const userAgent = 'Request-Promise-Native/1.0.5';
   // Format query string for logging debug information
-  Object.keys(queryStrings).forEach(k => {
-    strQueryString += `${strQueryString === '' ? '?' : '&'}${k}=${queryStrings[k]}`;
-  });
-  MFBGB.Logger.debug(`GET: ${uri}`);
-  MFBGB.Logger.debug(`${strQueryString === '' ? '(no query string)': strQueryString}`);
-  MFBGB.Logger.debug(`user-agent: ${USERAGENT}, parseJson: ${parseJson}`);
+  const strQueryString = querystring.stringify(queryStrings);
 
-  return rpn({uri: uri, qs: queryStrings, headers: {'User-Agent': USERAGENT}, json: parseJson}).then(res => {
+  MFBGB.Logger.debug(`GET: ${uri}`);
+  MFBGB.Logger.debug(`${strQueryString === '' ? '(no query string)': '?' + strQueryString}`);
+  MFBGB.Logger.debug(`user-agent: ${userAgent}, parseJson: ${parseJson}`);
+
+  return rpn({uri: uri, qs: queryStrings, headers: {'User-Agent': userAgent}, json: parseJson}).then(res => {
     MFBGB.Logger.debug(`GET: Successful`);
     return res;
   }).catch(e => MFBGB.Logger.error(`GET: Failed: ${e}`));
