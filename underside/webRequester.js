d1fcb988671861a9e32d730aceb6580a4c9eb5dd
MaySoMusician <maysomusician@gmail.com>
2019-02-08 15:53:46 +0900

    Add fetch command to webRequester with cheerio-httpcli

--- underside/webRequester.js
+++ underside/webRequester.js
@@ -1,6 +1,7 @@
 /* eslint-disable one-var */
 const querystring = require('querystring'),
-      rpn = require('request-promise-native');
+      rpn = require('request-promise-native'),
+      ch = require('cheerio-httpcli');
       // USERAGENT = 'Request-Promise-Native/1.0.5';
 
 const get = (MFBGB, uri, queryStrings, parseJson) => {
@@ -18,6 +19,26 @@ const get = (MFBGB, uri, queryStrings, parseJson) => {
   }).catch(e => MFBGB.Logger.error(`|WebRequester| Get: Failed: ${e}`));
 };
 
+const fetch = (MFBGB, uri, queryStrings, encode = undefined) => {
+  const userAgent = 'Cheerio-Httpcli/0.7.3';
+  ch.reset();
+  ch.set('headers', {'User-Agent': userAgent});
+  ch.set('referer', false);
+
+  // Format query string for logging debug information
+  const strQueryString = querystring.stringify(queryStrings);
+
+  MFBGB.Logger.debug(`|WebRequester| Fetch: ${uri}`);
+  MFBGB.Logger.debug(`|WebRequester| ${strQueryString === '' ? '(no query string)': '?' + strQueryString}`);
+  MFBGB.Logger.debug(`|WebRequester| user-agent: ${userAgent}`);
+
+  return ch.fetch(uri, queryStrings, encode).then(res => {
+    MFBGB.Logger.debug(`|WebRequester| Fetch: Successful`);
+    return res;
+  }).catch(e => MFBGB.Logger.error(`|WebRequester| Fetch: Failed: ${e}`));
+};
+
 module.exports = {
   'get': get,
+  'fetch': fetch,
 };
